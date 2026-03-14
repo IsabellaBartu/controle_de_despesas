91216c67c55bfe5cebbb24a27588c95c5163a422
IsabellaBartu <isabellamarques1008@gmail.com>
2026-03-14 00:30:18 -0300

    conectando SQlite e criando tabelas

--- criarbd.py
+++ criarbd.py
@@ -1,2 +1,21 @@
-#import sqlite
+# Importando SQlite
 import sqlite3 as lite
+
+# Criando conexão
+con = lite.connect('dados.db')
+
+# Criando tabelas
+# 1. Categoria
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Categoria(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT) ")
+
+# 2. Receitas 
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Receitas(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, adicionado_em DATE, valor DECIMAL) ")
+
+# 3. Gastos
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Gastos(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, retirado_em DATE, valor DECIMAL) ")
